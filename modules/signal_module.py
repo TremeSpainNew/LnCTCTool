b38091fa43abc6a50c6ea26059ea32a51818b6c7
TremeSpain <tremeempresa@gmail.com>
2026-05-15 08:24:08 +0200

    fixed

--- modules/signal_module.py
+++ modules/signal_module.py
@@ -3,6 +3,7 @@
 from PySide6.QtWidgets import QMenu
 from PySide6.QtCore import Qt, QRectF, QPointF
 from PySide6.QtGui import QPen, QColor, QBrush
+import math
 
 from constants import (
     MODULE_W, MODULE_H, ModuleKind, Direction,
@@ -45,6 +46,9 @@ class SignalModule(BaseModule):
         # vía
         painter.setBrush(QBrush(QColor("black")))
         painter.drawRect(QRectF(0, y - 5, MODULE_W, 10))
+        # sensores / retroseñalización
+        self.draw_slot_on_line(painter, 0, y, MODULE_W, y, t=0.25)
+        self.draw_slot_on_line(painter, 0, y, MODULE_W, y, t=0.75)
 
         # botón central
         painter.setBrush(QBrush(QColor("#888888")))
@@ -98,6 +102,9 @@ class SignalModule(BaseModule):
         # vía
         painter.setBrush(QBrush(QColor("black")))
         painter.drawRect(QRectF(0, y - 5, MODULE_W, 10))
+        # sensores / retroseñalización
+        self.draw_slot_on_line(painter, 0, y, MODULE_W, y, t=0.25)
+        self.draw_slot_on_line(painter, 0, y, MODULE_W, y, t=0.75)
 
         # botón central
         painter.setBrush(QBrush(QColor("#888888")))
@@ -160,6 +167,27 @@ class SignalModule(BaseModule):
                 lamp_radius
             )
 
+    
+    def draw_slot_on_line(self, painter, x1, y1, x2, y2, t=0.5, w=24, h=5):
+        x = x1 + (x2 - x1) * t
+        y = y1 + (y2 - y1) * t
+
+        angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
+
+        painter.save()
+        painter.translate(x, y)
+        painter.rotate(angle)
+
+        painter.setPen(QPen(QColor("black"), 1))
+        painter.setBrush(QBrush(QColor("white")))
+        painter.drawRoundedRect(
+            QRectF(-w / 2, -h / 2, w, h),
+            2,
+            2
+        )
+
+        painter.restore()
+    
     # ==========================================================
     # MENU
     # ==========================================================
